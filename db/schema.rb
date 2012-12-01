e4fd8837e4c7d0dd50014740006a1ea1c9cd770b
Henry S <henrythe9th@gmail.com>
2012-12-01 16:53:49 -0500

    Further work on OmniAuth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201200213) do
+ActiveRecord::Schema.define(:version => 20121201201216) do
 
   create_table "posts", :force => true do |t|
     t.datetime "created_at",     :null => false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(:version => 20121201200213) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
