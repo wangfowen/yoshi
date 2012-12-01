d83b712df90fd14240bc8e5f21e402683cc864ba
Owen Wang <wangfowen@gmail.com>
2012-12-01 12:55:57 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yoshi::Application.config.secret_token = 'fb1833f3eff50b0a47c9f71396b4586baeb83a0bc350e66dc0b5c2093c48b918b8ff4b1745066f87dac3f5637bb6618fb57702cd5efff25eb678656208a0c599'
