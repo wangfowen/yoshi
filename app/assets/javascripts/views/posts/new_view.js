57d30348d3f145ec99fc4123352d9016d11506eb
Henry S <henrythe9th@gmail.com>
2012-12-01 23:09:56 -0500

    Added check for current user in Backbone views. Need to implement current_user check

--- app/assets/javascripts/views/posts/new_view.js
+++ app/assets/javascripts/views/posts/new_view.js
@@ -15,31 +15,36 @@ views.PostsNewView = Backbone.View.extend({
   
   },
   createPost: function(e) {
-  	e.stopPropagation();
-  	e.preventDefault();
+    if (true) {
+      e.stopPropagation();
+      e.preventDefault();
 
-  	var $spinner = $('.spinner');
+      var $spinner = $('.spinner');
 
-  	$spinner.css("display", "inline-block");
+      $spinner.css("display", "inline-block");
 
-  	this.model.set({
-      post: {
-    		title: $('#title').val(),
-    		category: $('#category').val(),
-    		description: $('#description').val(),
-    		candidate_name: $('#candidate_name').val(),
-        candidate_email: $('#candidate_email').val(),
-    		deadline: $('#deadline').val()
-      }
-  	});
+      this.model.set({
+        post: {
+          title: $('#title').val(),
+          category: $('#category').val(),
+          description: $('#description').val(),
+          candidate_name: $('#candidate_name').val(),
+          candidate_email: $('#candidate_email').val(),
+          deadline: $('#deadline').val()
+        }
+      });
 
-  	this.model.save({}, {
-  		success: function() {
-  			window.location = "/?success=1";
-  		},
-  		error: function() {
-  			alertify.error("An error occurred when saving");
-  		}
-  	});
+      this.model.save({}, {
+        success: function() {
+          window.location = "/?success=1";
+        },
+        error: function() {
+          alertify.error("An error occurred when saving");
+        }
+      });
+    }
+    else {
+      alertify.error("Please Sign in first before posting a new interview");
+    } 
   }
 });
