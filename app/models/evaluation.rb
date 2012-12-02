dd0ffe54ab5516fc746a140dac40dc80206b0f00
Owen Wang <wangfowen@gmail.com>
2012-12-02 12:29:48 -0500

    finished interview flow

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,6 +1,6 @@
 class Evaluation < ActiveRecord::Base
 	attr_accessible :post_id, :rating, :comments, :recommended, :evaluator_id, :completed
-	attr_accessor :completed, :read
+
 	belongs_to :post
 	belongs_to :evaluator, class_name: "User"
 
