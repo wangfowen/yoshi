bec33fbc9948df8ac83a829ab80aa6456dc7bc22
Owen Wang <wangfowen@gmail.com>
2012-12-02 01:57:11 -0500

    user profiles

--- app/presenters/posts_presenter.rb
+++ app/presenters/posts_presenter.rb
@@ -13,6 +13,6 @@ class PostsPresenter
     user = User.find(@post.user_id)
     created_at = @post.created_at.nil? ? nil : @post.created_at.strftime("%D")
     deadline = @post.deadline.nil? ? nil : @post.deadline.strftime("%D")
-    @post.attributes.except("created_at", "deadline").merge("created_at" => created_at, "deadline" => deadline, "user" => user.name, "user_id" => user.id)
+    @post.attributes.except("created_at", "deadline").merge("created_at" => created_at, "deadline" => deadline, "user" => user.name, "user_id" => user.id, "profile_pic_url" => user.profile_pic_url)
   end
 end
