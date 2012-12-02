b5dc26d16648cd0336f31b359538b3b70d274b82
Henry S <henrythe9th@gmail.com>
2012-12-01 22:36:22 -0500

    Added logout and login urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Yoshi::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" } do
     #get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
+    get 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   resources :posts
