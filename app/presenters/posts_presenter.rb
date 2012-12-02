a3e46434e1c7cdb06726716d453bbf69a2755757
Owen Wang <wangfowen@gmail.com>
2012-12-02 11:09:00 -0500

    pmy posts logic is alot better now

--- app/presenters/posts_presenter.rb
+++ app/presenters/posts_presenter.rb
@@ -14,11 +14,20 @@ class PostsPresenter
     created_at = @post.created_at.nil? ? nil : @post.created_at.strftime("%D")
     deadline = @post.deadline.nil? ? nil : @post.deadline.strftime("%D")
     
-    application = Application.find_by_post_id(@post.id)
-    application_status = application.nil? ? -1 : application.booked? ? application.applicant_id : false
+    applications = Application.find_all_by_post_id(@post.id)
+    application_status = applications.select { |application| !application.booked? }
+
+    if application_status.empty?
+      application_status = false
+    else
+      application_status = application_status.first.applicant_id
+    end
+
+    num_applicants = applications.count
     
     @post.attributes.except("created_at", "deadline").merge("created_at" => created_at, 
       "deadline" => deadline, "user" => user.name, "user_id" => user.id, 
-      "profile_pic_url" => user.profile_pic_url, "application_status" => application_status)
+      "profile_pic_url" => user.profile_pic_url, "application_status" => application_status,
+      "num_applicants" => num_applicants)
   end
 end
